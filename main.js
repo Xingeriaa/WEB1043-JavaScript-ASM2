7023523aebb12d90ba46eb3da120c733b699169c
Xing <116251968+Xingeriaa@users.noreply.github.com>
2024-08-02 02:51:09 +0700

    3AM

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const BANNER_COUNT = 5;
+const INTERVAL_DURATION = 5000;
+
+var CurrentProgress = 0;
+var CurrentBanner = 1;
+var BannerAutomation = null;
+var ProgressAutomation = null;
 
 const ProductsList = {
     1: {name: "Kê tay công thái học DeltaHub Carpio G2.0", price: "1.100.000đ", src: "./src/Products/DeltaHub-Carpio-G2.webp"},
@@ -34,25 +41,16 @@ const BrandsList = {
     18: "./src/Brand/ninjutso.avif",
     19: "./src/Brand/skypad.webp",
     20: "./src/Brand/glorious.avif"
-
 }
 
 const BannersList = {
     1: {src: "./src/Banner/banner1.webp", about: "Lót chuột kính cường lực giới hạn", name: "Yuki Aim 2024 Katana & Kitsune", button: "Xem thêm"},
-    2: {src: "./src/Banner/banner2.webp", about: "Lót chuột kính cường lực giới hạn", name: "Yuki Aim 2024 Katana & Kitsune", button: "Xem thêm"},
-    3: {src: "./src/Banner/banner3.webp", about: "Lót chuột kính cường lực giới hạn", name: "Yuki Aim 2024 Katana & Kitsune", button: "Xem thêm"},
-    4: {src: "./src/Banner/banner4.webp", about: "Lót chuột kính cường lực giới hạn", name: "Yuki Aim 2024 Katana & Kitsune", button: "Đặt ngay"},
-    5: {src: "./src/Banner/banner5.jpg", about: "Lót chuột kính cường lực giới hạn", name: "Yuki Aim 2024 Katana & Kitsune", button: "Xem chi tiết"}
+    2: {src: "./src/Banner/banner2.webp", about: "Lót chuột PORON cao cấp chính hãng", name: "Fnatic MAX và Lethal Gaming", button: "Xem thêm"},
+    3: {src: "./src/Banner/banner3.webp", about: "Thương hiệu gaming gear cao cấp hiệu năng cao", name: "Vancer - Công nghệ LCD dongle độc quyền", button: "Xem thêm"},
+    4: {src: "./src/Banner/banner4.webp", about: "Thiết kế bởi Phong Cách Xanh", name: "Túi đựng bàn phím/ gaming gear", button: "Đặt ngay"},
+    5: {src: "./src/Banner/banner5.jpg", about: "Lót chuột siêu chậm, siêu control", name: "Lethal Gaming Gear Jupiter PRO", button: "Xem chi tiết"}
 }
 
-function ImagesPreloader(src) {
-    Object.keys(src).forEach(key => {
-        const value = src[key];
-        var Img = new Image();
-        Img.src = src;
-    });
-}
- 
 function AddProduct(name,price,src) {
     let ProductSeciton = document.querySelector(".products-section");
     let Product = `<div class="product-frame">
@@ -69,29 +67,47 @@ function AddBrand(src) {
     BrandSection.innerHTML += Brand;
 }
 
+function InfoSwticher(title, desc, button) {
+    const Title = document.querySelector(".desc-section h1");
+    const Description = document.querySelector(".desc-section h4");
+    const Button = document.querySelector(".desc-section input");
+
+    Title.style.transition = "opacity 0.3s";
+    Description.style.transition = "opacity 0.3s";
+    Button.style.transition = "opacity 0.3s";
+
+    Title.style.opacity = 0;
+    Description.style.opacity = 0;
+    Button.style.opacity = 0;
+
+    setTimeout(() => {
+        Title.innerHTML = title;
+        Description.innerHTML = desc;
+        Button.value = button;
+
+        Title.style.opacity = 1;
+        Description.style.opacity = 1;
+        Button.style.opacity = 1;
+    }, 300);
+}
+
 function BannerSwitcher(src) {
     const Banner = document.querySelector(".background-img");
-    const Title = document.querySelector(".desc-section h1");
-    const Description = document.querySelector(".desc-section h4")
-    const Button = document.querySelector(".desc-section input");
-    let duration = 300;
-    let steps = 60; 
-    let interval = duration / steps;
 
-    for (let i = steps; i > 0; i--) {
-        setTimeout(() => {
-            Banner.style.filter = `brightness(${i}%)`;
-        }, interval * (steps - i));
-    }
+    Banner.style.transition = "filter 0.3s";
+    Banner.style.filter = "brightness(0%)";
 
     setTimeout(() => {
         Banner.style.backgroundImage = `url(${src})`;
-        for (let i = 0; i < steps; i++) {
-            setTimeout(() => {
-                Banner.style.filter = `brightness(${i + 1}%)`;
-            }, interval * i);
-        }
-    }, duration);
+        Banner.style.filter = "brightness(65%)";
+    }, 300);
+}
+
+function PagesSwitcher(Index) {
+    let PagesSection = document.querySelectorAll(".pages-btn");
+    PagesSection.forEach(Child => {
+        Child.style.color = (Child.value == Index) ? "white" : "gray";
+    });
 }
 
 Object.keys(ProductsList).forEach(key => {
@@ -104,12 +120,259 @@ Object.keys(BrandsList).forEach(key => {
     AddBrand(value);
 });
 
-var CurrentBanner = 1;
-
-setInterval(() => {
+BannerAutomation = setInterval(() => {
     CurrentBanner++;
-    if (CurrentBanner > 5) {
+    if (CurrentBanner > BANNER_COUNT) {
         CurrentBanner = 1
     }
     BannerSwitcher(BannersList[CurrentBanner].src);
-}, 3000);
+    InfoSwticher(BannersList[CurrentBanner].name, BannersList[CurrentBanner].about, BannersList[CurrentBanner].button)
+    PagesSwitcher(CurrentBanner);
+}, INTERVAL_DURATION);
+
+ProgressAutomation = setInterval(() => {
+    if (CurrentProgress >= 100) {
+        CurrentProgress = 0;
+    } else {
+        CurrentProgress++;
+    }
+    const progressBar = document.querySelector(".progress-bar");
+    if (progressBar) {
+        progressBar.style.width = CurrentProgress + "%";
+    }
+}, INTERVAL_DURATION/100);
+
+document.querySelector("header").addEventListener("mousemove", function() {
+    let RightCursor = document.querySelector("#right");
+    let LeftCursor = document.querySelector("#left");
+    RightCursor.style.zIndex = "0";
+    LeftCursor.style.zIndex = "0";
+});
+
+document.querySelector("header").addEventListener("mouseout", function() {
+    let RightCursor = document.querySelector("#right");
+    let LeftCursor = document.querySelector("#left");
+    RightCursor.style.zIndex = "2";
+    LeftCursor.style.zIndex = "2";
+});
+
+document.querySelector("header").childNodes.forEach(function(Child){
+    Child.addEventListener("mousemove", function() {
+        let RightCursor = document.querySelector("#right");
+        let LeftCursor = document.querySelector("#left");
+        RightCursor.style.zIndex = "0";
+        LeftCursor.style.zIndex = "0";
+    });
+    
+    Child.addEventListener("mouseout", function() {
+        let RightCursor = document.querySelector("#right");
+        let LeftCursor = document.querySelector("#left");
+        RightCursor.style.zIndex = "2";
+        LeftCursor.style.zIndex = "2";
+    });
+});
+
+document.querySelector(".desc-section input").addEventListener("mouseover", function() {
+    let RightCursor = document.querySelector("#right");
+    let LeftCursor = document.querySelector("#left");
+    RightCursor.style.zIndex = "0";
+    LeftCursor.style.zIndex = "0";
+});
+
+document.querySelector(".desc-section input").addEventListener("mouseout", function() {
+    let RightCursor = document.querySelector("#right");
+    let LeftCursor = document.querySelector("#left");
+    RightCursor.style.zIndex = "2";
+    LeftCursor.style.zIndex = "2";
+});
+
+document.querySelector("header").addEventListener("mousemove", function() {
+    let RightCursor = document.querySelector("#right");
+    let LeftCursor = document.querySelector("#left");
+    RightCursor.style.zIndex = "0";
+    LeftCursor.style.zIndex = "0";
+});
+
+document.querySelector("header").addEventListener("mouseout", function() {
+    let RightCursor = document.querySelector("#right");
+    let LeftCursor = document.querySelector("#left");
+    RightCursor.style.zIndex = "2";
+    LeftCursor.style.zIndex = "2";
+});
+
+
+document.querySelectorAll(".pages-btn").forEach(Child => {
+    Child.addEventListener("mouseover", function() {
+        let RightCursor = document.querySelector("#right");
+        let LeftCursor = document.querySelector("#left");
+        RightCursor.style.zIndex = "0";
+        LeftCursor.style.zIndex = "0";
+    });
+})
+
+document.querySelectorAll(".pages-btn").forEach(Child => {
+    Child.addEventListener("mouseout", function() {
+        let RightCursor = document.querySelector("#right");
+        let LeftCursor = document.querySelector("#left");
+        RightCursor.style.zIndex = "2";
+        LeftCursor.style.zIndex = "2";
+    });
+})
+
+document.querySelectorAll(".pages-btn").forEach(Child => {
+    Child.addEventListener("click", function() {
+        clearInterval(BannerAutomation);
+        clearInterval(ProgressAutomation);
+        document.querySelector(".progress-bar").style.transition = "none"
+        CurrentProgress = 0;
+        document.querySelector(".progress-bar").style.transition = "50ms linear"
+        CurrentBanner = Child.value;
+        BannerSwitcher(BannersList[CurrentBanner].src);
+        InfoSwticher(BannersList[CurrentBanner].name, BannersList[CurrentBanner].about, BannersList[CurrentBanner].button);
+        PagesSwitcher(CurrentBanner);
+
+        BannerAutomation = setInterval(() => {
+            CurrentBanner++;
+            if (CurrentBanner > BANNER_COUNT) {
+                CurrentBanner = 1;
+            }
+            BannerSwitcher(BannersList[CurrentBanner].src);
+            InfoSwticher(BannersList[CurrentBanner].name, BannersList[CurrentBanner].about, BannersList[CurrentBanner].button);
+            PagesSwitcher(CurrentBanner);
+        }, INTERVAL_DURATION);
+
+        ProgressAutomation = setInterval(() => {
+            if (CurrentProgress >= 100) {
+                CurrentProgress = 0;
+            } else {
+                CurrentProgress++;
+            }
+            const progressBar = document.querySelector(".progress-bar");
+            if (progressBar) {
+                progressBar.style.width = CurrentProgress + "%";
+            }
+        }, INTERVAL_DURATION/100);        
+    });
+});
+
+document.querySelectorAll(".categories-container div").forEach(Child => {
+    let img = Child.querySelector("img");
+    let p = Child.querySelector("p");
+    img.addEventListener("mouseover", function() {
+        img.style.transform = "scale(1.1)";
+        p.style.bottom = "12%";
+    });
+});
+
+document.querySelectorAll(".categories-container div").forEach(Child => {
+    let img = Child.querySelector("img");
+    let p = Child.querySelector("p");
+    img.addEventListener("mouseout", function() {
+        img.style.transform = "scale(1)";
+        p.style.bottom = "15%";
+    });
+});
+
+document.querySelector(".banner-container").addEventListener("mousemove", function(event) {
+    let RightCursor = document.querySelector("#right");
+    let LeftCursor = document.querySelector("#left");
+    const posX = event.clientX;
+    const posY = event.clientY;
+
+    if (posX > (window.innerWidth/2)) {
+        LeftCursor.style.top = -100 + 'px';
+        RightCursor.style.top = posY + 'px';
+        RightCursor.style.left = posX + 'px';
+    } else {
+        RightCursor.style.top = -100 + 'px';
+        LeftCursor.style.top = posY + 'px';
+        LeftCursor.style.left = posX + 'px';
+    }
+});
+
+document.querySelector(".banner-container").addEventListener("mouseout", function(event) {
+    let RightCursor = document.querySelector("#right");
+    let LeftCursor = document.querySelector("#left");
+
+    RightCursor.style.top = -100 + 'px';
+    LeftCursor.style.left = -100 + 'px';
+});
+
+document.querySelector(".banner-container").addEventListener("click", function(event) {
+    const posX = event.clientX;
+    if (posX > (window.innerWidth/2)) {
+
+        clearInterval(BannerAutomation);
+        clearInterval(ProgressAutomation);
+        document.querySelector(".progress-bar").style.transition = "none"
+        CurrentProgress = 0;
+        document.querySelector(".progress-bar").style.transition = "50ms linear"
+        CurrentBanner++;
+        if (CurrentBanner > BANNER_COUNT) {
+            CurrentBanner = 1;
+        }
+        BannerSwitcher(BannersList[CurrentBanner].src);
+        InfoSwticher(BannersList[CurrentBanner].name, BannersList[CurrentBanner].about, BannersList[CurrentBanner].button);
+        PagesSwitcher(CurrentBanner);
+
+        BannerAutomation = setInterval(() => {
+            CurrentBanner++;
+            if (CurrentBanner > BANNER_COUNT) {
+                CurrentBanner = 1;
+            }
+            BannerSwitcher(BannersList[CurrentBanner].src);
+            InfoSwticher(BannersList[CurrentBanner].name, BannersList[CurrentBanner].about, BannersList[CurrentBanner].button);
+            PagesSwitcher(CurrentBanner);
+        }, INTERVAL_DURATION);
+
+        ProgressAutomation = setInterval(() => {
+            if (CurrentProgress >= 100) {
+                CurrentProgress = 0;
+            } else {
+                CurrentProgress++;
+            }
+            const progressBar = document.querySelector(".progress-bar");
+            if (progressBar) {
+                progressBar.style.width = CurrentProgress + "%";
+            }
+        }, INTERVAL_DURATION/100);        
+
+    } else {
+
+        clearInterval(BannerAutomation);
+        clearInterval(ProgressAutomation);
+        document.querySelector(".progress-bar").style.transition = "none"
+        CurrentProgress = 0;
+        document.querySelector(".progress-bar").style.transition = "50ms linear"
+        CurrentBanner--;
+        if (CurrentBanner < 1) {
+            CurrentBanner = BANNER_COUNT;
+        }
+        BannerSwitcher(BannersList[CurrentBanner].src);
+        InfoSwticher(BannersList[CurrentBanner].name, BannersList[CurrentBanner].about, BannersList[CurrentBanner].button);
+        PagesSwitcher(CurrentBanner);
+
+        BannerAutomation = setInterval(() => {
+            CurrentBanner--;
+            if (CurrentBanner > BANNER_COUNT) {
+                CurrentBanner = 1;
+            }
+            BannerSwitcher(BannersList[CurrentBanner].src);
+            InfoSwticher(BannersList[CurrentBanner].name, BannersList[CurrentBanner].about, BannersList[CurrentBanner].button);
+            PagesSwitcher(CurrentBanner);
+        }, INTERVAL_DURATION);
+
+        ProgressAutomation = setInterval(() => {
+            if (CurrentProgress >= 100) {
+                CurrentProgress = 0;
+            } else {
+                CurrentProgress++;
+            }
+            const progressBar = document.querySelector(".progress-bar");
+            if (progressBar) {
+                progressBar.style.width = CurrentProgress + "%";
+            }
+        }, INTERVAL_DURATION/100);    
+
+    }
+});
